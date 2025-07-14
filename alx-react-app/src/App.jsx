7d4d11bd94016d1f27474c8b8d3c100650806c37
Yildum David Jonah <dyildum@gmail.com>
2025-07-14 22:24:49 +0100

    Completed all React fundamentals tasks

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
