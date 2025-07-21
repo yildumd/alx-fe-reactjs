090b32c742efd39bfe4205ec77ffd45b9eb7a5ff
Yildum David Jonah <dyildum@gmail.com>
2025-07-21 22:50:00 +0100

    Update components

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com", bio: "One simple man for one simple task." };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <UserContext.Provider value={{userData}} >
+      <ProfilePage />
+    </UserContext.Provider>
   )
 }
 
-export default App
+export default App;
---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import UserContext from "./UserContext"
+
+const UserProfile = () => {
+
+    const {userData} = useContext(UserContext);
+
+    return (
+    <div>
+       <h2>{userData.name}</h2>
+       <p>Age: {userData.age}</p>
+       <p>Bio: {userData.bio}</p>
+    </div>
+    )
+}
+
+export default UserProfile
