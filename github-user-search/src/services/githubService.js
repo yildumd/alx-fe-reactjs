da59a5ca1d2460e42e6634ad7afb48d682681212
Yildum David Jonah <dyildum@gmail.com>
2025-08-03 01:02:41 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
-    headers: {
-      'Accept': 'application/vnd.github.v3+json'
-    }
-  });
-  return response;
+export const searchUsers = async (username, location, minRepos) => {
+  try {
+    // Build the query string with all parameters
+    let query = `${username}`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>${minRepos}`;
+    
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`, {
+      headers: {
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    });
+    return response.data.items;
+  } catch (error) {
+    throw error;
+  }
 };
