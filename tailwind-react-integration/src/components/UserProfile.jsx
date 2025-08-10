1a657b06e6632623088c719d14af9fcda76d3c5e
Yildum David Jonah <dyildum@gmail.com>
2025-08-10 23:22:48 +0100

    Final responsive fix: added sm:w-24 for image

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
       />
       <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-3 sm:my-4">
         John Doe
