692bbaac51376bae6dc4cf5996453a420dd31b65
Yildum David Jonah <dyildum@gmail.com>
2025-08-10 23:19:10 +0100

    Fix all responsive requirements: added md:max-w-sm and md:text-xl

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
         className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-3 sm:my-4">
         John Doe
       </h1>
       <p className="text-gray-600 text-sm sm:text-base">
